31fca939cb211e208f6e4c38a0434517c05789da
SaraDoesIt <sclark@foghornconsulting.com>
2024-06-07 10:25:13 -0400

    Update navbar and it's styling.

--- base.py
+++ base.py
@@ -8,19 +8,19 @@ app = Flask(__name__)
 # home page
 @app.route('/')
 def home():
-    return render_template("home.html", title="Home")
+    return render_template("home.html")
 
 # resume page
 @app.route('/resume')
 def resume():
-    return render_template("resume.html", title="Resume")
+    return render_template("resume.html")
 
 # lors page
 @app.route('/lors')
 def lors():
-    return render_template("lors.html", title="LORs")
+    return render_template("lors.html")
 
 # projects page
 @app.route('/projects')
 def projects():
-    return render_template("projects.html", title="Projects")
+    return render_template("projects.html")
